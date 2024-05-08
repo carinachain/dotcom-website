fd2e37de0b5fa978413deb3485f01886cd485faf
Hoshimiya Yuzuki <hoshimiya@Hoshimiyas-MacBook-Air.local>
2024-05-08 23:15:47 +0900

    feat: theme bug fix

--- src/app/[locale]/(unauth)/layout.tsx
+++ src/app/[locale]/(unauth)/layout.tsx
@@ -1,12 +1,16 @@
 // import Link from 'next/link';
+'use client';
+
 import { useTranslations } from 'next-intl';
+import { ThemeProvider } from '@mui/system';
+import theme from '@/app/theme';
 
 export default function Layout(props: { children: React.ReactNode }) {
   const t = useTranslations('RootLayout');
 
   return (
-    <>
-    {props.children}
-    </>
+    <ThemeProvider theme={theme}>
+      {props.children}
+    </ThemeProvider>
   );
 }
