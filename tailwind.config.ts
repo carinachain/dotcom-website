31ebe85e4b6ac576f9440b54d4a3840c1b33810d
Hoshimiya Yuzuki <hoshimiya@Hoshimiyas-MacBook-Air.local>
2024-05-16 23:34:18 +0900

    feat: footer layout optimize

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,21 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+// import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        body: [
+          'Hiragino Sans',
+          'ヒラギノ角ゴシック',
+          'メイリオ',
+          'Meiryo',
+          'MS Ｐゴシック',
+          'MS PGothic',
+          'sans-serif',
+          'YuGothic',
+          'Yu Gothic',
+        ],
       },
       backgroundImage: {
         'carousel-bg': "url('/images/images/index_head_bg.png')",
