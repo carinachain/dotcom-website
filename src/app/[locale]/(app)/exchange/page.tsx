f24705be8222d6cbdcc6880ec261bac9aa1b00c4
KurumiFrost <3081555424@qq.com>
2024-06-09 01:31:22 +0900

    feat: optimize of home, points and exchange pages

--- src/app/[locale]/(app)/exchange/page.tsx
+++ src/app/[locale]/(app)/exchange/page.tsx
@@ -12,8 +12,8 @@ export default function Points() {
 const Carousel = function () {
   return (
     <div className="flex justify-center">
-      <div className="w-[1200px] grid grid-cols-1 md:grid-cols-2 bg-slate-300 h-[500px] rounded-none md:rounded-t-[50px] overflow-hidden">
-        <div className="flex justify-center flex-col gap-4 px-14">
+      <div className="w-[1200px] grid grid-cols-1 md:grid-cols-2 bg-slate-300 h-[500px] rounded-none md:rounded-t-[50px] relative overflow-hidden">
+        <div className="flex justify-center flex-col gap-3 px-14 translate-y-[-50px]">
           <div className="text-2xl">积分/优惠券互通互换</div>
           <div className="text-2xl">自由灵活，各取所需</div>
           <div className="text-orange-500 w-[400px]">
@@ -23,6 +23,12 @@ const Carousel = function () {
         <div className="hidden md:block">
           <div className="bg-[url('/images/function_interchange_head_images_01.png')] bg-cover w-full h-[550px] -translate-y-10"></div>
         </div>
+        <div className="absolute bg-black bg-cover w-[30px] h-[30px] left-[550px] top-[50px] rounded-full z-50"></div>
+        <div className="absolute bg-black bg-cover w-[30px] h-[30px] left-[80px] bottom-[30px] rounded-full z-50"></div>
+        <div className="absolute bg-white bg-cover w-[30px] h-[30px] left-[340px] bottom-[40px] rounded-full z-50"></div>
+        <div className="absolute bg-orange-500 bg-cover w-[30px] h-[30px] left-[410px] bottom-[70px] rounded-full z-50"></div>
+        <div className="absolute bg-white bg-cover w-[120px] h-[120px] left-[380px] bottom-[-60px] rounded-full z-50"></div>
+        <div className="absolute bg-white bg-cover w-[80px] h-[80px] left-[110px] top-[-40px] rounded-full z-50"></div>
       </div>
     </div>
   )
@@ -31,13 +37,12 @@ const Carousel = function () {
 
 // Feature block
 const Feature = function () {
-    // const t = useTranslations('home.feature');
     return (
       <div className="flex justify-center">
         <div className="w-[1200px]">
           <div className="flex justify-center text-2xl pt-[50px] pb-[40px]">Carina Chain 灵活互通互換</div>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-color-2 mb-[100px]">
-            <div className="flex flex-col items-center w-full h-[550px] bg-orange-400 rounded-none md:rounded-[50px]">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-color-2 mb-[100px] drop-shadow-2xl">
+            <div className="flex flex-col items-center w-full h-[550px] bg-orange-400 rounded-none md:rounded-[50px] drop-shadow">
               <div className="h-[250px] flex justify-center items-center">
                 <div className="h-[160px] w-[160px] bg-[url(/images/function_interchange_icon_01.png)] bg-no-repeat bg-contain"></div>
               </div>
@@ -46,9 +51,9 @@ const Feature = function () {
                 <div className="mb-[15px]">用户可以将持有的积分/优惠券赠子其他用户</div>
               </div>
             </div>
-            <div className="flex flex-col items-center w-full h-[550px] bg-orange-400 rounded-none md:rounded-[50px]">
+            <div className="flex flex-col items-center w-full h-[550px] bg-orange-400 rounded-none md:rounded-[50px] drop-shadow">
               <div className="h-[250px] flex justify-center items-center">
-                <div className="h-[160px] w-[160px] bg-[url(/images/function_interchange_icon_01.png)] bg-no-repeat bg-contain"></div>
+                <div className="h-[160px] w-[160px] bg-[url(/images/function_interchange_icon_02.png)] bg-no-repeat bg-contain"></div>
               </div>
               <div className="text-white text-2xl mb-[20px]">互换（跳蛋市场）</div>
               <div className="px-[30px]">
@@ -56,9 +61,9 @@ const Feature = function () {
                 <div className="mb-[15px]">其他用户在市场中看到自己想交换的积分/优惠券后即可以进行交换。</div>
               </div>
             </div>
-            <div className="flex flex-col items-center w-full h-[550px] bg-orange-400 rounded-none md:rounded-[50px]">
+            <div className="flex flex-col items-center w-full h-[550px] bg-orange-400 rounded-none md:rounded-[50px] drop-shadow">
               <div className="h-[250px] flex justify-center items-center">
-                <div className="h-[160px] w-[160px] bg-[url(/images/function_interchange_icon_01.png)] bg-no-repeat bg-contain"></div>
+                <div className="h-[160px] w-[160px] bg-[url(/images/function_interchange_icon_03.png)] bg-no-repeat bg-contain"></div>
               </div>
               <div className="text-white text-2xl mb-[20px]">兑换</div>
               <div className="px-[30px]">
--- src/app/[locale]/(app)/points/page.tsx
+++ src/app/[locale]/(app)/points/page.tsx
@@ -11,17 +11,30 @@ export default function Points() {
 const Carousel = function () {
     return (
         <div className="flex justify-center">
-            <div className="w-[1200px] grid grid-cols-1 md:grid-cols-2 bg-slate-300 h-[500px] rounded-none md:rounded-t-[50px] bg-orange-400/80">
-                <div className="flex justify-center flex-col gap-4 px-10">
+            <div className="w-[1200px] grid grid-cols-1 md:grid-cols-2 bg-orange-300 h-[500px] rounded-none md:rounded-t-[50px] bg-orange-400/80 relative overflow-hidden">
+                <div className="flex justify-center flex-col gap-3 px-10">
                     <div className="text-2xl">发行您商铺的专属积分/优惠券</div>
                     <div className="text-2xl">只需几个歩驟和1枚硬市（500日元）</div>
-                    <div className="text-white w-[350px]">
-                        账户中的积分/优惠券可以与其他用户进行交换，以物易物让手中的积分/优惠券更自由，更灵活有用同时积分/优惠券的发行商户也扩大了自己的用户群
+                    <div className="text-white w-[380px]">
+                    满足各类零售业商户的需求，发行商户定制的积分/优惠券<br />
+                    建立自己的客户群<br />
+                    激励客户再度光顾，提高客户的忠实度<br />
+                    帮助您扩大商铺的营业额<br />
                     </div>
                 </div>
-                <div className="hidden md:block">
-                    <div className="bg-[url('/images/function_exclusive_head_image_01.png')] bg-cover w-[400px] h-full"></div>
+                <div className="hidden md:block relative">
+                    <div className="absolute left-[50px] bottom-0 bg-[url('/images/function_exclusive_head_image_01.png')] bg-cover w-[400px] h-full z-50"></div>
+                    <div className="absolute rounded-t-[50px] h-[480px] w-[450px] bg-slate-400/80 left-0 bottom-0 z-10"></div>
                 </div>
+                <div className="absolute bg-[url('/images/function_common_yen_bg.png')] bg-cover w-[60px] h-[60px] left-[580px] top-[70px] z-50"></div>
+                <div className="absolute bg-[url('/images/function_exclusive_yen_bg.png')] bg-cover w-[40px] h-[40px] right-[80px] bottom-[-15px] z-50"></div>
+                <div className="absolute bg-black bg-cover w-[30px] h-[30px] left-[550px] top-[50px] rounded-full z-50"></div>
+                <div className="absolute bg-black bg-cover w-[30px] h-[30px] left-[80px] bottom-[30px] rounded-full z-50"></div>
+                <div className="absolute bg-white bg-cover w-[30px] h-[30px] left-[340px] bottom-[40px] rounded-full z-50"></div>
+                <div className="absolute bg-orange-500 bg-cover w-[30px] h-[30px] left-[410px] bottom-[70px] rounded-full z-50"></div>
+                <div className="absolute bg-white bg-cover w-[120px] h-[120px] left-[380px] bottom-[-60px] rounded-full z-50"></div>
+                <div className="absolute bg-white bg-cover w-[120px] h-[120px] left-[630px] bottom-[160px] rounded-full z-30"></div>
+                <div className="absolute bg-white bg-cover w-[80px] h-[80px] left-[110px] top-[-40px] rounded-full z-50"></div>
             </div>
         </div>
     )
@@ -45,14 +58,14 @@ const Feature = function () {
                     <div className="grow flex flex-col items-center justify-center">
                         <div className="text-3xl mb-[40px]">1硬币定制发行，0日元维持</div>
                         <div className="text-gray-500 flex flex-col items-cneter justify-center gap-1">
-                            <div>设定自己的参数名称/简介等后即可发行专厲积分或优惠券</div>
-                            <div>发行积分或优惠券仅需1枚500日元硬币</div>
-                            <div>可按需增发或销毀，只需2日元/次的手续费，无任何维持费用</div>
+                            <div className="text-center">设定自己的参数名称/简介等后即可发行专厲积分或优惠券</div>
+                            <div className="text-center">发行积分或优惠券仅需1枚500日元硬币</div>
+                            <div className="text-center">可按需增发或销毀，只需2日元/次的手续费，无任何维持费用</div>
                         </div>
                     </div>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8 pt-[80px] px-[20px] lg:px-[80px] text-gray-800">
-                    <div className="bg-gray-100 min-h-[400px] flex flex-col rounded-xl">
+                    <div className="bg-white min-h-[400px] flex flex-col rounded-xl shadow-lg">
                         <div className="bg-[url('/images/function_exclusive_white_pic_01.png')] bg-cover h-[160px] m-[20px] rounded-xl">
                         </div>
                         <div className="m-[20px]">
@@ -73,7 +86,7 @@ const Feature = function () {
                             </div>
                         </div>
                     </div>
-                    <div className="bg-gray-100 min-h-[400px] flex flex-col rounded-md">
+                    <div className="bg-white min-h-[400px] flex flex-col rounded-md shadow-lg">
                         <div className="bg-[url('/images/function_exclusive_white_pic_02.png')] bg-cover bg-blue-100 h-[160px] m-[20px] rounded-md">
                         </div>
                         <div className="m-[20px]">
@@ -91,7 +104,7 @@ const Feature = function () {
                             </div>
                         </div>
                     </div>
-                    <div className="bg-gray-100 min-h-[400px] flex flex-col rounded-md">
+                    <div className="bg-white min-h-[400px] flex flex-col rounded-md shadow-lg">
                         <div className="bg-[url('/images/function_exclusive_white_pic_03.png')] bg-cover bg-blue-100 h-[160px] m-[20px] rounded-md">
                         </div>
                         <div className="m-[20px]">
